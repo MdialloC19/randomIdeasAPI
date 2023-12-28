0848a86a81131c6c85f364ccfcf33a77016e0e6a
MdialloC19 <mdiallo3211@gmail.com>
2023-12-28 23:59:23 +0000

    frontend added

---
+++ client/webpack.config.js
@@ -0,0 +1,56 @@
+const path =require('path');
+const HTMLWebpackPlugin=require('html-webpack-plugin');
+const MiniCssExtractPlugin=require('mini-css-extract-plugin');
+
+module.exports= {
+    mode: 'development',
+    entry: './src/index.js',
+    output:{
+       path: path.resolve(__dirname,'../public'),
+       filename: 'bundle.js'
+    },
+    module :{
+        rules:[
+            { 
+                test: /\.css$/, 
+                use:[MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: [
+                        ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                    }
+                }
+            }
+        ],
+    },
+    plugins:[
+        new HTMLWebpackPlugin({
+            title :'Webpack Starter',
+            filename:'index.html',
+            template:'./src/index.html',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, '../public'),
+        },
+        port: 3000,
+        open:true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    }
+
+
+
+
+
+
+}
