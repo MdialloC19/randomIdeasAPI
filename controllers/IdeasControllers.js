96a347350abc6522359f82ea0d05ab06da0b9186
MdialloC19 <mdiallo3211@gmail.com>
2024-01-06 12:11:07 +0000

    adding class controller and new route configurations ok

--- controllers/IdeasControllers.js
+++ controllers/IdeasControllers.js
@@ -1,9 +1,7 @@
 const Idea=require('../models/Idea');
 const {isValidObjectId}=require('mongoose');
 
-
-
-exports.getAllIdeas= async (res,req)=>{
+exports.getAllIdeas= async (req,res)=>{
         try {
             const ideas= await Idea.find();
             res.status(200).json({
@@ -11,13 +9,16 @@ exports.getAllIdeas= async (res,req)=>{
                 data:ideas
             }); 
         } catch (error) {
-            res.status(500).json({ succeed: false, error: 'Something went wrong' });
+            res.status(500).json({
+                succed:false,
+                error: 'Sommething went Wrong!!!'
+            });
             console.log(error)
         }
         
     }
 
-exports.getIdea  =async (res,req)=>{
+exports.getIdea=async (req,res)=>{
         try {
             const ideaId=req.params.id;
             if(!isValidObjectId(ideaId)){
@@ -26,14 +27,12 @@ exports.getIdea  =async (res,req)=>{
                     error :'Invalid Id '
                 });
             }
-
             const idea=await Idea.findById(ideaId);
             if(idea){
                 return res.status(404).json({
                     succeed:false,
                     error :'Idea not found'
                 });
-
             }
             res.status(200).json({
                 succeed: true,
@@ -49,7 +48,7 @@ exports.getIdea  =async (res,req)=>{
         }
     }
 
-exports.postIdea=async (res,req)=>{
+exports.postIdea=async (req,res)=>{
     const { text, tag, username } = req.body;
     const idea = new Idea({ text, tag, username });
     try {
@@ -64,9 +63,99 @@ exports.postIdea=async (res,req)=>{
             succed:false,
             error: 'Something went Wrong'
         });
-        
-
         console.log(error);
-        
+    }
+}
+
+exports.putIdea=async (req,res)=>{
+    try {
+        const ideaId= req.params.id;
+        const idea=await Idea.findById(ideaId);
+
+        if (!isValidObjectId(ideaId)) {
+            return res.status(400).json({
+                succeed: false,
+                error: 'Invalid Idea ID'
+            });
+        }
+
+        if(idea.user===req.params.username){
+            const { text, tag, username } = req.body;
+            const updateFields = {};
+
+            if (text) updateFields.text = text;
+            if (tag) updateFields.tag = tag;
+            if (username) updateFields.username = username;
+
+            if(Object.keys(updateFields).length===0){
+                return res.status(400).json({
+                    succed: false,
+                    error : 'No field to update'
+                });
+            }
+
+            const updatedIdea=await Idea.findByIdAndUpdate({
+                id:ideaId,
+                $set:updateFields,
+                new:true
+            });
+
+            if (updatedIdea === null) {
+                return res.status(404).json({ succeed: false, error: 'Idea not found' });
+            }
+            res.status(200).json({ succeed: true, data: updatedIdea });
+
+        }else{
+            res.status(403).json({
+                succed: false,
+                error: 'you are not autorized to delete this ressource'
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            succed:false,
+            error:'Something went wrong'+ error.message
+        });
+        console.log(error);
+    }
+}
+
+exports.deletedIdea=async (req,res)=>{
+    try {
+        const ideaId=req.params.id;
+        if(!isValidObjectId(ideaId)){
+            return res.status(400).json({
+                succed:false,
+                error : 'Invalid Id'
+            });
+        }
+        const idea=await Idea.findById(ideaId);
+
+        if(!idea){
+            return res.json(404).json({
+                succed:false,
+                error: 'Idea not found !!!'
+            });
+        }
+
+        if(idea.username===req.body.username){
+            const deletedIdea =await Idea.findByIdAndDelete(ideaId);
+            res.status(200).json({
+                succed:true,
+                data:deletedIdea
+            });
+
+        }else{
+            res.status(403).json({
+                succed: false,
+                error: 'you are not autorized to delete this ressource'
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            succed:false,
+            error:'Something went wrong'+ error.message
+        });
+       console.log(error);
     }
 }
--- routes/ideas.js
+++ routes/ideas.js
@@ -1,86 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Idea = require('../models/Idea');
-const mongoose = require('mongoose');
-const { isValidObjectId } = require('mongoose');
 const IdeasControllers = require('../controllers/IdeasControllers');
 
 router.get('/', IdeasControllers.getAllIdeas);
 router.get('/:id', IdeasControllers.getIdea);
 router.post('/', IdeasControllers.postIdea);
-/**
- * @route PUT /ideas/:id
- * @description Update an existing idea by its ID
- * @access Public
- */
-
-
-router.put('/:id', async (req, res) => {
-
-    try {
-        const ideaId = req.params.id;
-        const idea=await Idea.findById(ideaId);
-        if(idea.username===req.body.username){
-
-            const { text, tag, username } = req.body;
-            const updateFields = {};
-        
-            if (!isValidObjectId(ideaId)) {
-                return res.status(400).json({ succeed: false, error: 'Invalid Idea ID' });
-            }
-            if (text) updateFields.text = text;
-            if (tag) updateFields.tag = tag;
-            if (username) updateFields.username = username;
-        
-            if (Object.keys(updateFields).length === 0) {
-                return res.status(400).json({ succeed: false, error: 'No fields to update' });
-            }
-            const updatedIdea = await Idea.findOneAndUpdate(
-                { _id: ideaId },
-                { $set: updateFields },
-                { new: true }
-            );
-            if (updatedIdea === null) {
-                return res.status(404).json({ succeed: false, error: 'Idea not found' });
-            }
-            res.status(200).json({ succeed: true, data: updatedIdea });
-        }else{
-            // Username don't match
-            res.status(403).json({ succeed: false, error: 'you are not autorized to delete this ressource' });
-        }
-       
-    } catch (error) {
-        res.status(500).json({ succeed: false, error: error.message });
-    }
-});
-
-
-
-/**
- * @route DELETE /ideas/:id
- * @description Delete an idea by its ID
- * @access Public
- */
-router.delete('/:id', async (req, res) => {
-    try {
-        const idea=await Idea.findById(req.params.id);
-        if(idea.username===req.body.username){
-            const ideaId = req.params.id;
-            if (!isValidObjectId(ideaId)) {
-                return res.status(400).json({ succeed: false, error: 'Invalid idea ID' });
-            }
-            const deletedIdea = await Idea.findOneAndDelete({ _id: ideaId });
-            if (!deletedIdea) {
-                return res.status(404).json({ succeed: false, error: 'Idea not found' });
-            }
-            res.status(200).json({ succeed: true, data: deletedIdea });
-        }else{
-            res.status(403).json({ succeed: false, error: 'you are not autorized to delete this ressource' });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ succeed: false, error: error.message });
-    }
-});
+router.put('/:id',IdeasControllers.putIdea);
+router.delete('/:id',IdeasControllers.deletedIdea);
 
 module.exports = router;
